e4e0023c4c25f3b7182fb5293250854f6b4d3671
Matt Valli <matthew.valli@gmail.com>
2022-11-13 21:23:34 +0700

    Initialize project using Create React App

---
+++ .storybook/decorators/DecorateThemeProvider.tsx
@@ -0,0 +1,22 @@
+// @ts-ignore
+import { getTheme } from '@agrippa-io/storybook-mui-5/dist/themes/utils/getTheme'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import {
+  THEMES_MUI_5,
+  defaultTheme,
+} from '../../src/styles/themes/Matx/MatxThemes'
+
+// @ts-ignore
+export const DecorateThemeProvider = (Story, context) => {
+  const theme = getTheme(context, THEMES_MUI_5, defaultTheme)
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story {...context} />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  )
+}
