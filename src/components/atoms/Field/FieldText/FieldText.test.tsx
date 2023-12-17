bbaf2dd14a47fa944e77eeb6e5d0c44b3cc961c4
Matt Valli <matthew.valli@gmail.com>
2023-12-16 23:36:18 -0800

    Moved lintstaged configuration out of package.json to .lintstagedrc

--- src/components/atoms/Field/FieldText/FieldText.test.tsx
+++ src/components/atoms/Field/FieldText/FieldText.test.tsx
@@ -3,5 +3,6 @@
 describe("Components - Atoms - Field - FieldText", () => {
   it("should run in lint staged commit", () => {
     expect(true).toEqual(true);
+    expect(true).toEqual(true);
   });
 });
