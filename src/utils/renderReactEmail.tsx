8b22c09558293d08d611c591ccc1ccf110e06313
Matt Valli <matthew.valli@gmail.com>
2024-01-30 00:25:27 -0800

    removed ts-ignore statement

--- src/utils/renderReactEmail.tsx
+++ src/utils/renderReactEmail.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentType } from 'react'
 
-// @ts-ignore
 import { renderEmail } from 'react-html-email'
 
 export const renderReactEmail = (Component: ComponentType<any>, data: any) => {
